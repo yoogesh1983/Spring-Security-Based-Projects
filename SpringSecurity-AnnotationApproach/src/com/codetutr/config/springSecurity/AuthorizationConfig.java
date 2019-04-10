74f86a168d5387c355d3c9723faadf2ae4e34a87
yoogesh1983@gmail.com <yoogesh1983@gmail.com>
2019-04-10 16:55:20 -0400

    Updating Access Decision Manager

--- SpringSecurity-AnnotationApproach/src/com/codetutr/config/springSecurity/AuthorizationConfig.java
+++ SpringSecurity-AnnotationApproach/src/com/codetutr/config/springSecurity/AuthorizationConfig.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
 
@@ -17,18 +19,24 @@ public class AuthorizationConfig {
 	@Bean
     public AccessDecisionManager getAccessDecisionManager()
     {
-    	DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
-    	expressionHandler.setRoleHierarchy(getRoleHierarchy());
-    	
-    	WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
-    	webExpressionVoter.setExpressionHandler(expressionHandler);
-    	
     	List<AccessDecisionVoter<? extends Object>> voters = new ArrayList<>();
-    	
-    	voters.add(webExpressionVoter);
+    	voters.add(getwebExpressionVoter());
     	return new AffirmativeBased(voters);
     }
 	
+	@Bean
+	public WebExpressionVoter getwebExpressionVoter() {
+		WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+		webExpressionVoter.setExpressionHandler(getExpressionHandler());
+		return webExpressionVoter;
+	}
+	
+	@Bean
+	public SecurityExpressionHandler<FilterInvocation> getExpressionHandler() {
+		DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
+		expressionHandler.setRoleHierarchy(getRoleHierarchy());
+		return expressionHandler;
+	}
 	
 	@Bean
 	public RoleHierarchy getRoleHierarchy() {
