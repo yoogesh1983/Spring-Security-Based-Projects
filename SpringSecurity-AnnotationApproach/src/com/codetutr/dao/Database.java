080f0cd2739e906286732e099f88a56f8642c80f
yoogesh1983@gmail.com <yoogesh1983@gmail.com>
2019-04-11 15:43:12 -0400

    Changed username and password for mock users

--- SpringSecurity-AnnotationApproach/src/com/codetutr/dao/Database.java
+++ SpringSecurity-AnnotationApproach/src/com/codetutr/dao/Database.java
@@ -13,9 +13,9 @@ public class Database
 	
 	static {
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		profileTable.put(1L, new Profile(1L, "syoogesh@gmail.com",passwordEncoder.encode("Countrr1"), "Yoogesh", "Sharma", "ROLE_USER"));
-		profileTable.put(2L, new Profile(2L, "kristy@gmail.com",passwordEncoder.encode("Countrr1"), "Kristy", "Sharma", "ROLE_ADMIN"));
-		profileTable.put(3L, new Profile(3L, "sushila@gmail.com",passwordEncoder.encode("Countrr1"), "Sushila", "Sapkota", "ROLE_DBA"));
+		profileTable.put(1L, new Profile(1L, "user@gmail.com",passwordEncoder.encode("1234"), "Yoogesh", "Sharma", "ROLE_USER"));
+		profileTable.put(2L, new Profile(2L, "admin@gmail.com",passwordEncoder.encode("1234"), "Kristy", "Sharma", "ROLE_ADMIN"));
+		profileTable.put(3L, new Profile(3L, "dba@gmail.com",passwordEncoder.encode("1234"), "Sushila", "Sapkota", "ROLE_DBA"));
 	}
 	
 	public static Map<Long, Profile> getProfileTable() 
