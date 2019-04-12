7069718f2fdaf3726bfccc79826c1c54ee07b171
yoogesh <yoogesh1983@gmail.com>
2019-04-12 12:08:40 -0400

    comment

--- SpringSecurity-AnnotationApproach/src/com/codetutr/config/springSecurity/SpringSecurityInitializer.java
+++ SpringSecurity-AnnotationApproach/src/com/codetutr/config/springSecurity/SpringSecurityInitializer.java
@@ -4,9 +4,7 @@ import org.springframework.security.web.context.AbstractSecurityWebApplicationIn
 
 /**
  * This is done to register the {@link springSecurityFilterChain} with the war.Not suprisingly, Spring Security provides a base class 
- * {@link AbstractSecurityWebApplicationInitializer} that will ensure the {@link springSecurityFilterChain} gets registered for you
- * 
- * @author yoogesh
+ * {@link AbstractSecurityWebApplicationInitializer} that will ensure the {@link springSecurityFilterChain} gets registered for you. <p>
  */
 public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
