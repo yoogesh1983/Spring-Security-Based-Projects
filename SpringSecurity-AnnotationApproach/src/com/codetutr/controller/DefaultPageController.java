b5f392734bf47303d7ab7bb7e67922acd3fd142a
Yoogesh <yoogesh1983@gmail.com>
2019-04-30 20:55:18 -0400

    update

--- SpringSecurity-AnnotationApproach/src/com/codetutr/controller/DefaultPageController.java
+++ SpringSecurity-AnnotationApproach/src/com/codetutr/controller/DefaultPageController.java
@@ -10,14 +10,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.codetutr.config.logging.Log;
 import com.codetutr.services.ProfileService;
 
 @Controller
 public class DefaultPageController 
-{
-	private static final String CLASS_NAME = DefaultPageController.class.getName();
-	
+{	
 	@Autowired
 	ProfileService profileservice;
 	
@@ -28,7 +25,7 @@ public class DefaultPageController
 	
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
 	public String homePage(ModelMap model, HttpServletRequest request, HttpServletResponse response) 
-	{Log.logInfo(CLASS_NAME, "init()" ,"Initlamoarted" );
+	{
 		return "security/sign-in";
 	}
 
