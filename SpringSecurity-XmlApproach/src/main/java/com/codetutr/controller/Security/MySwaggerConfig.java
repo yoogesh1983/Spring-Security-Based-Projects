6cb03f82fe31565cbd9352ecca3e73f5ffeeece0
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2020-09-30 09:38:29 -0400

    adding OpenAPI 3

--- SpringSecurity-XmlApproach/src/main/java/com/codetutr/controller/Security/MySwaggerConfig.java
+++ SpringSecurity-XmlApproach/src/main/java/com/codetutr/controller/Security/MySwaggerConfig.java
@@ -1,9 +1,17 @@
 package com.codetutr.controller.Security;
+import org.springdoc.webmvc.core.SpringDocWebMvcConfiguration;
+import org.springdoc.webmvc.ui.SwaggerConfig;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
+@Import({ org.springdoc.core.SpringDocConfiguration.class, SpringDocWebMvcConfiguration.class,
+    SwaggerConfig.class, org.springdoc.core.SwaggerUiConfigProperties.class,
+    org.springdoc.core.SwaggerUiOAuthProperties.class,
+    org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration.class, org.springdoc.core.SpringDocConfigProperties.class })
 public class MySwaggerConfig {
 
+	
 }
